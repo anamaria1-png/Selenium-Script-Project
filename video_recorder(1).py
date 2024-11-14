333b4120c2472d5e082aa62a1766ff8b9bba026c
Ana-Maria Puscasu <89720718+anamaria1-png@users.noreply.github.com>
2024-11-14 19:47:22 +0200

    Add files via upload

---
+++ video_recorder(1).py
@@ -0,0 +1,43 @@
+import cv2
+import numpy as np
+import pyautogui
+import time
+
+
+resolution = pyautogui.size()
+codec = cv2.VideoWriter_fourcc(*'mp4v')
+filename = "video.mp4"
+fps = 5
+out = cv2.VideoWriter(filename, codec, fps, resolution)
+
+
+def record_video(sec):
+    # Video settings
+    RECORD_SEC = sec  # Specify how many seconds to record
+    """Records the screen as video for a specified duration."""
+    cv2.namedWindow("Recording", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("Recording", 640, 480)
+    
+    start_time = time.time()  # Track the start time
+    try:
+        while time.time() - start_time < RECORD_SEC:
+            screenshot = pyautogui.screenshot()
+            frame = np.array(screenshot)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            out.write(frame)
+            cv2.imshow("Recording", frame)
+
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+            time.sleep(1 / fps)
+    except Exception as e:
+        print(f"Error during video recording: {e}")
+    finally:
+        close_video_recorder()
+
+def close_video_recorder():
+    """Closes the video recording resources."""
+    out.release()
+    cv2.destroyAllWindows()
+    print("Video recording complete.")
+
